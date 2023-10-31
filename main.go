c79628015749d5ef5e6ca26517bc4d93fa78bb00
victor.tseng <withvictor@gmail.com>
2023-10-31 09:23:01 +0800

    資料分類

--- main.go
+++ main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
+	"fmt"
 	"sync"
-	"tpl/control"
 	"tpl/db"
 )
 
@@ -28,24 +28,46 @@ func main() {
 	//    2.run laravel docker
 	//    3.run laravel controller database model router sidemenu
 	//建立假資料
-	lara := control.InitFakeData()
+	// lara := control.InitFakeData()
 	//測試
 	//control.TestGenLaravel()
 	//建立laravel DB資料
-	control.GetTbGenerateMigrateTable(lara.ProjectID, lara.ProjectName)
+	// control.GetTbGenerateMigrateTable(lara.ProjectID, lara.ProjectName)
+
 	// control.GetTbGenerateMigrateTable(61, "isb32")
 	//清除測試資料
 	//	control.TrancateData()
+	rece()
 }
 
 func createdb() {
 	db.CreateOrRecreateDB(dbname)
 }
 
+type Animal struct {
+	Field     string
+	ShowName  string
+	Migrate   string
+	ModelType string
+	Require   string
+}
+
 func rece() {
-	var strs []string
-	for msg := range status {
-		//fmt.Println(msg)
-		strs = append(strs, msg)
+	data := map[string][]Animal{
+		"cats": {
+			{"name", "名字", "aa", "aa", "aa"},
+			{"sex", "text", "aa", "aa", "aaaa"},
+		},
+		"cats1": {
+			{"name", "text", "aa", "aa", "aa"},
+			{"name", "text", "aaaa", "aa", "aa"},
+		},
+	}
+
+	for a, b := range data {
+		fmt.Println(a, b)
+		for v, d := range b {
+			fmt.Println(v, d.Field)
+		}
 	}
 }
